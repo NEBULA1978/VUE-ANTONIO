6d9ce4a90d5dbe4ec5874709c9e296656f7e546b
NEBULA1978 <rpascualey@gmail.com>
2023-07-21 21:57:03 +0200

    Maquetado html y main.css

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,13 +1,83 @@
 <template>
-<div>
-  Hello world
-</div>
+  <nav class="navbar">
+    <img src="./assets/logo.svg" width="50" />
+    <div class="brand">Todo List App</div>
+  </nav>
+
+  <main class="container">
+    <section>
+      <form class="add-todo-form">
+        <input type="text" placeholder="Todo Title" />
+  <div>
+    <button style="background: var(--accent-color); color: black;">Add Todo</button>
+  </div>
+
+
+      </form>
+    </section>
+
+    <section>
+      <div class="todo">
+        <p>Pasear al perro</p>
+        <div>
+          <button class="remove-todo-btn">&times;</button>
+        </div>
+      </div>
+    </section>
+  </main>
 </template>
 
-<script setup>
+<script>
 
 </script>
 
 <style scoped>
+.navbar {
+  display: flex;
+  align-items: center;
+  background: var(--navbar-color);
+  padding: 20px;
+  margin-bottom: 30px;
+}
 
+.brand {
+  font-size: 2rem;
+}
+
+.add-todo-form {
+  display: flex;
+  justify-content: space-between;
+}
+
+.add-todo-form input {
+  width: 80%;
+  border: solid 2px var(--accent-color);
+}
+
+.add-todo-form button {
+  background: var(--accent-color);
+  color: var(--text-color);
+  border: none;
+  height: 50px;
+}
+
+.todo {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: var(--accent-color);
+  margin-top: 30px;
+  padding: 0 20px 0 20px;
+  border-radius: 10px;
+}
+
+.remove-todo-btn {
+  border-radius: 50%;
+  border: none;
+  height: 40px;
+  width: 40px;
+  font-size: 30px;
+  color: var(--text-color);
+  background: var(--danger-color);
+}
 </style>
